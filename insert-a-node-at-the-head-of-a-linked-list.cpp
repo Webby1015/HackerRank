e6f2b61c06b96d50558c473649131342938d0374
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-02-19 21:39:13 +0530

    Create insert-a-node-at-the-head-of-a-linked-list.cpp

---
+++ insert-a-node-at-the-head-of-a-linked-list.cpp
@@ -0,0 +1,22 @@
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* llist, int data, int position) {
+    SinglyLinkedListNode * node = new SinglyLinkedListNode(data);
+    SinglyLinkedListNode * head = llist;
+    if(position == 0){
+        node->next = head;
+        return node;
+    }
+
+    SinglyLinkedListNode * afterPosition = head;
+    SinglyLinkedListNode * beforePosition = head;
+    head = head->next;
+    while(position-- >= 1){
+        beforePosition = head;
+        head = head->next;
+        afterPosition = head;
+        if(position == 1){
+            beforePosition->next = node;
+            node->next = afterPosition; 
+        }
+    }
+    return llist;
+}
